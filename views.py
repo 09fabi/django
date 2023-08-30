af2d36bdf8afb6ed2b5e14759d0bca8746ebba07
Fabián <129874104+09fabi@users.noreply.github.com>
2023-08-30 16:52:19 -0400

    30-08

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# django-admin startproject (nombre proyecto)
+# o
+# python - m django startproject (nombre proyecto)
+# luego crear app para el proyecto
+
+# crea app para el proyecto
+# python manage.py startapp (nombre app)
